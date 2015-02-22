f554fefe28fe7b06aefdac0d372a472ae1392c0e
Yunus YEN <yunusyenigor@hotmail.com>
2015-02-21 17:15:55 -0800

    NodeJS + Express framework used

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var port = 8888;
+
+function start(){
+	
+	var express = require('express');
+	var app = express();
+
+	 app.get('/about', function (req, res) {
+	  res.send('Hello World!');
+	})
+
+	app.use(express.static(__dirname + '/public'));
+	app.listen(port, function() {
+	    console.log('server listening on port ' + port);
+	});
+}
+exports.start = start;
