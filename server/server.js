964a706e713e5426607181d63ba7398e3d9dd372
Yunus YEN <yunusyenigor@hotmail.com>
2016-06-17 14:50:07 -0700

    port place

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express 	= require("express"),
 	path 		= require('path'),
-	// port        = Number(process.env.PORT || 3000),
+	port        = Number(process.env.PORT || 3000);
 	app 		= express(),
 	bodyParser 	= require("body-parser");
 	
@@ -9,17 +9,7 @@ var express 	= require("express"),
 var twitter 	= require("./modules/twitter")(app),
 	api         = require("./api")(app);
 
-app.set('port', (process.env.PORT || 5000));
-
 function start(){
-
-	// // Run the Server
-	// app.listen(port, function() {
-	//     console.log('Server listening on port: ' + port);
-	// });
-	app.listen(app.get('port'), function() {
-        console.log('Node app is running on port', app.get('port'));
-    });
 
 	var folder = __dirname + '../../public';
 	app.use("/", express.static(folder));
@@ -41,7 +31,9 @@ function start(){
 	  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 	}
 
-	//function(req, res){var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;console.log(res);}
-
+	// Run the Server
+	app.listen(port, function() {
+	    console.log('Server listening on port: ' + port);
+	});
 }
 exports.start = start;
